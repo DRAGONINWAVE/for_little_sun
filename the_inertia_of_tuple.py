02267cfd3c56d30713407bbf219dac3af1a72ecf
DRAGONINWAVE <yl597591@gmail.com>
2022-03-22 09:47:25 +0800

    the factual use of unpacking tuple

---
+++ the_inertia_of_tuple.py
@@ -0,0 +1,13 @@
+metro_areas=[('Tokyo','JP',36.933,(35.689722,139.691667)),('Delhi NCR','IN',21.935,(28.613889,77.208889)),
+             ('Mexico City','MX',20.142,(19.433333,-99.133333)),('New York-Newark','US',20.104,(40.808611,-74.020386)),
+             ('São Paulo','BR',19.649,(-23.547778,-46.635833)),]
+
+def main():
+    print(f'{"":15} | {"latitude":>9} | {"longitude":>9}')
+    for city,_,_,(lat,lon) in metro_areas:
+        if lon < 0:
+            print(f'{city:15} | {lat:>9.5f} | {lon:>9}')
+
+if __name__ == main():
+    main()
+
