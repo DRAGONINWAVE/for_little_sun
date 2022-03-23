d964a2645945c9898535a6da5c5f08278571dde9
DRAGONINWAVE <yl597591@gmail.com>
2022-03-23 10:28:58 +0800

    simple slice

---
+++ simple_slice.py
@@ -0,0 +1,9 @@
+a = list(range(10))
+print(a[:3]) #end in third
+print(a[3:]) #begin in fourth
+#the two parts are added as a whole
+
+b = 'bicycle'
+print(b[::3])
+print(b[::-1]) #reserved
+print(b[::-2]) #fun ha?
