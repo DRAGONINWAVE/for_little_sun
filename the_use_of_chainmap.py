4213a6a01aee1b252f13f259a730ee76ca5f80dd
DRAGONINWAVE <yl597591@gmail.com>
2022-03-27 11:05:46 +0800

    the few functions in collections

--- the_use_of_chainmap.py
+++ the_use_of_chainmap.py
@@ -1,3 +1,4 @@
+import collections
 from collections import ChainMap
 d1 = {'a':1,'b':2,'c':3}
 d2 = {'a':2,'b':3,'c':4,'d':5}
@@ -5,3 +6,9 @@ chain = ChainMap(d1, d2)
 print(chain['a'])
 chain['c'] = 233
 print(chain)
+import builtins
+pylookup = ChainMap(locals(),globals(),vars(builtins))
+# print(pylookup)
+ct = collections.Counter('abracadabra')
+print(ct)
+print(ct.most_common(3))
