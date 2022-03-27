1382b267e274312aded7758519a9c7b55ccc4e2b
DRAGONINWAVE <yl597591@gmail.com>
2022-03-27 10:41:30 +0800

    uses dict.setdefault to fetch and update a list of word occurrences from the index in a single line

--- index0.py
+++ index0.py
@@ -1,13 +1,14 @@
 import re
 import sys
 
-WORD_RE = re.compile(r'\w+')
+WORD_RE = re.compile(r'\w+') # get an object  of regular expression by re.compile
 
 index = {}
 with open(sys.argv[0], encoding='utf-8') as fp:
+    # print(fp.read())
     for line_no,line in enumerate(fp, 1):
         for match in WORD_RE.finditer(line):
-            word = match.group()
+            word = match.group() #whole word
             column_no = match.start() + 1
             location = (line_no, column_no)
             # # ugly?
@@ -18,3 +19,4 @@ with open(sys.argv[0], encoding='utf-8') as fp:
 
 for word in sorted(index, key=str.upper):
     print(word, index[word])
+print(index)
