23c999deed8e81ae1f6a4cbd98e0af14b33ed6b7
DRAGONINWAVE <yl597591@gmail.com>
2022-04-02 16:23:48 +0800

    帮助锋子哥进行一个误差数据的去

--- help_others/dataclear-9999.py
+++ help_others/dataclear-9999.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 import openpyxl
 BF = pd.read_excel(r'D:\piles\JAN.influence_factors.xlsx',dtype=np.float)
-BF.replace('-9999',' ')
-BF.to_excel(r'C:\Users\Administrator\Desktop\dataclear.xlsx',index=False)
+CF = BF.replace(-9999,' ')
+CF.to_excel(r'C:\Users\Administrator\Desktop\dataclear.xlsx',index=False)
