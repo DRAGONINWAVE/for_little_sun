742575dad03e97509a8d55404e59965851fd4cdc
DRAGONINWAVE <yl597591@gmail.com>
2022-04-07 15:59:07 +0800

    核密度图优化，pandas,scipy还是要多学习啊

--- help_others/核密度图_优化.py
+++ help_others/核密度图_优化.py
@@ -7,31 +7,44 @@ import time
 
 
 start_time = time.time()
+season = ['春','夏','秋','冬']
 # #导入数据
 # raw_data = pd.read_excel(r'D:\piles\冬季温度及其各类影响指数.xlsx')
 # #清理数据，删除所有存在空白数据的行
 # cleared_data = raw_data.dropna()
 #保存数据到当前文件夹
-cleared_data = pd.read_excel('冬季温度及其各类影响指数_c1.xlsx')
-# JAN_NDVI_c = cleared_data['JAN_NDVI_c'] #自变量
-# JAN_NDBI_C = cleared_data['JAN_NDBI_C'] #自变量
-# JAN_MNDWI_ = cleared_data['JAN_MNDWI_'] #自变量
-y = cleared_data.iloc[:,4].values  #因变量
-# print(type(y),y)
+# for i in range(4):
+#     path = 'D:\\piles\\'+season[i]+'季温度及其各类影响指数.xlsx'
+#     raw_data = pd.read_excel(path)
+#     cleared_data = raw_data.dropna()
+#     cleared_data.to_excel(season[i]+'季温度及其各类影响指数优.xlsx')
+# end_time = time.time()
+# # print(end_time-start_time)
+#
+#
+# # JAN_NDVI_c = cleared_data['JAN_NDVI_c'] #自变量
+# # JAN_NDBI_C = cleared_data['JAN_NDBI_C'] #自变量
+# # JAN_MNDWI_ = cleared_data['JAN_MNDWI_'] #自变量
+# # y = cleared_data.iloc[:,4].values  #因变量
+# # print(type(y),y)
 
 #设置大的画图背景，为白色
 sns.set_theme(style='dark')
-f, axes = plt.subplots(3, 1, figsize=(9,18),
+f, axes = plt.subplots(3,4, figsize=(27,20),
                        # sharex = True
                        )
 i = 5
-k = 0
-for ax, s in zip(axes.flat, np.linspace(0, 3, 10)):
+k = 1
+l = 0
+for ax, s in zip(axes.flatten(order='F'), np.linspace(0, 3, 12)):
     # print(ax)
     plt.sca(ax)
     cmaps = sns.cubehelix_palette(start=s,light=1,as_cmap=True)
-    ls = ['JAN_NDVI_c','JAN_NDBI_C','JAN_MNDWI_']
-    x = cleared_data.iloc[:,i].values
+    season = ['春','夏','秋','冬']
+    cleared_data1 = pd.read_excel('D:\\TD\\help_others\\'+season[l]+'季温度及其各类影响指数优.xlsx')
+    ls = cleared_data1.columns.values[-4:]
+    y = cleared_data1.iloc[:,4].values  #因变量
+    x = cleared_data1.iloc[:,i].values
     df = pd.DataFrame(dict(x1 = x,JAN_tem=y))
     sns.kdeplot(
         x = 'x1' , y = 'JAN_tem',
@@ -41,7 +54,6 @@ for ax, s in zip(axes.flat, np.linspace(0, 3, 10)):
         fill = True,
         thresh=.2,
         ax=ax,
-        # x_label = x_label
     )
     x1_1 = list(x)
 
@@ -49,9 +61,17 @@ for ax, s in zip(axes.flat, np.linspace(0, 3, 10)):
     print(slope1, intercept1, r1, p1_1, std_err1)
     p = sns.regplot(x='x1', y='JAN_tem', data=df,ci=None,scatter=False,
                 label=f'y={slope1:.2f}*x+{intercept1:.2f}',
-                ax=axes[k]
+                ax=ax
                 )
     plt.xlabel(ls[k])
+    plt.ylabel(ls[0]+'℃')
+    p.legend()
+    k = k + 1
+    i = i + 1
+    if k == 4:
+      k = 1
+      l = l + 1
+      i = 5
     p.legend()
     sns.rugplot(df.x1,
                 color="b",
@@ -60,8 +80,9 @@ for ax, s in zip(axes.flat, np.linspace(0, 3, 10)):
                 vertical=True,
                  ax=ax,
                 color='b')
-    i = i + 1
-    k = k + 1
+
+ax.set(xlim=(-1, 1), ylim=(-12, 26))
+f.subplots_adjust(0, 0, 1, 1, .08, .08)
 kdeplot_fig = f.get_figure()
 kdeplot_fig.savefig(str(time.strftime("%Y%m%d%H%M%S")))
 end_time = time.time()
