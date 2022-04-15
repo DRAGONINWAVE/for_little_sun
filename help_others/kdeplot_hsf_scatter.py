562f2117b3791cfdc8eeabf75e450a82372c5990
DRAGONINWAVE <yl597591@gmail.com>
2022-04-15 16:14:01 +0800

    advanced kdeplot

--- help_others/kdeplot_hsf_scatter.py
+++ help_others/kdeplot_hsf_scatter.py
@@ -8,28 +8,30 @@ import os
 
 
 start_time = time.time()
-# season = ['春','夏','秋','冬']
-# 清理数据并保存到指定文件夹
+# # season = ['春','夏','秋','冬']
+# # 清理数据并保存到指定文件夹
 # raw_path = r'D:\piles\wuxi_four_seasons'
 # names = os.listdir(raw_path)
+# print(names)
 # for name in names:
 #     raw_data = pd.read_excel('D:\\piles\\wuxi_four_seasons\\'+name)
 #     cleared_data = raw_data.dropna()
 #     cleared_data.to_excel('D:\\TD\\help_others\\hsf_kdeplot\\'+name,index=False)
 
 #设置大的画图背景，为白色
-sns.set_theme(style='white')
-f, axes = plt.subplots(3,3, figsize=(27,27),
+sns.set_theme(style='dark')
+f, axes = plt.subplots(3,4,
+                       figsize=(36,27),
                        # sharex = True
                        )
 i = 4
 k = 1
 l = 0
-for ax, s in zip(axes.flatten(order='F'), np.linspace(2, 3, 10)):
+for ax, s in zip(axes.flatten(order='F'), np.linspace(0, 3, 12)):
     # print(ax)
     plt.sca(ax)
     cmaps = sns.cubehelix_palette(start=s,light=1,as_cmap=True)
-    season = ['Spring','Fall','Winter']
+    season = ['Spring','Summer','Fall','Winter']
     cleared_data1 = pd.read_excel('D:\\TD\\help_others\\hsf_kdeplot\\'+season[l]+'.xlsx')
     ls = cleared_data1.columns.values[-4:]
     y = cleared_data1.iloc[:,-4].values  #因变量
