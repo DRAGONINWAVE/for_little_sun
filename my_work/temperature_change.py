2181f8d7b5684c47c0fdeee741861dae604d5a14
DRAGONINWAVE <yl597591@gmail.com>
2022-04-20 23:18:39 +0800

    temperature_change

--- my_work/temperature_change.py
+++ my_work/temperature_change.py
@@ -1,35 +1,98 @@
 import os
 import pandas as pd
 import time
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 start_time = time.time()
 
 path = 'D:\TD\my_work\data\\'
 names  = os.listdir(path)
-for name in names:
-    df = pd.read_excel(path+name)
+for k in range(len(names)):
+    df = pd.read_excel(path+names[k])
     counts = 0
-    year = list(range(1967,2017))
+    s1_T = 0
+    s2_T = 0
+    s3_T = 0
+    s4_T = 0
+    year = []
     average_T = []
-    j = 0
-    # print(df.year.values)
-    for i in range(1966,2017):
-        average_temperature = 0
-        for days in range(1,366):
-            if i == df.year.values[counts]:
-                # print(df.year.values[counts])
-                average_temperature = average_temperature + df.Tmean.values[counts]
-                counts = counts + 1
-            else:
-                average_temperatureY = average_temperature/days
-                # print(average_temperature)
-                average_T.append(average_temperatureY)
-                counts = counts + 1
+    average_s1_T = []
+    average_s2_T = []
+    average_s3_T = []
+    average_s4_T = []
+    # print(len(df.year))
+    for i in range(1967,2017):
+        y_T = 0
+        day = 0
+        day1 = 0
+        day2 = 0
+        day3 = 0
+        day4 = 0
+        season1 = list(range(1,4))
+        season2 = list(range(4,7))
+        season3 = list(range(7,10))
+        season4 = list(range(10,13))
+        for days in range(1,31*12):
+            if counts == len(df.year):
                 break
-
-        # print(counts)
-    print(len(df.year),366*51,df.year.values[counts])
-
-
+            if i == df.year[counts]:
+                # print(df.year.values[counts])
+                y_T = y_T + df.Tmean[counts]
+                # print(df.year.values[counts])
+                if df.month[counts] in season1:
+                    s1_T = s1_T + df.Tmean[counts]
+                    day1 = day1 + 1
+                if df.month[counts] in season2:
+                    s2_T = s2_T + df.Tmean[counts]
+                    day2 = day2 + 1
+                if df.month[counts] in season3:
+                    s3_T = s3_T + df.Tmean[counts]
+                    day3 = day3 + 1
+                if df.month[counts] in season4:
+                    s4_T = s4_T + df.Tmean[counts]
+                    day4 = day4 + 1
+                counts = counts + 1
+                day = day + 1
+            else:
+                break
+            # print(day)
+        if day != 0:
+            y_T = y_T / day
+            s1_T = s1_T / day1
+            s2_T = s2_T / day2
+            s3_T = s3_T / day3
+            s4_T = s4_T / day4
+            average_T.append(y_T)
+            average_s1_T.append(s1_T)
+            average_s2_T.append(s2_T)
+            average_s3_T.append(s3_T)
+            average_s4_T.append(s4_T)
+            year.append(df.year.values[counts-1])
+            print(average_T)
+        # if counts == len(df.year):
+        #     break
+        # print(len(average_T))
+    if k == 0:
+        data0 = pd.DataFrame(dict(Year=year,Tmean=average_T,S1=average_s1_T,S2=average_s2_T,S3=average_s3_T,S4=average_s4_T))
+        # print(data0)
+        data0.to_excel('D:\TD\my_work\data1\\' + 'YearMean'+names[0],index=False)
+    if k == 1:
+        data1 = pd.DataFrame(dict(Year=year,Tmean=average_T,S1=average_s1_T,S2=average_s2_T,S3=average_s3_T,S4=average_s4_T))
+        data1.to_excel('D:\TD\my_work\data1\\' + 'YearMean'+names[1],index=False)
+    if k == 2:
+        data2 = pd.DataFrame(dict(Year=year,Tmean=average_T,S1=average_s1_T,S2=average_s2_T,S3=average_s3_T,S4=average_s4_T))
+        data2.to_excel('D:\TD\my_work\data1\\' + 'YearMean'+names[2],index=False)
+    if k == 3:
+        data3 = pd.DataFrame(dict(Year=year,Tmean=average_T,S1=average_s1_T,S2=average_s2_T,S3=average_s3_T,S4=average_s4_T))
+        data3.to_excel('D:\TD\my_work\data1\\' + 'YearMean'+names[3],index=False)
+    if k ==4 :
+        data4 = pd.DataFrame(dict(Year=year,Tmean=average_T,S1=average_s1_T,S2=average_s2_T,S3=average_s3_T,S4=average_s4_T))
+        data4.to_excel('D:\TD\my_work\data1\\' + 'YearMean'+names[4],index=False)
+    # print(data0.S1)
+    # print(name)
+    # sns.relplot(ls[i].Year, ls[i].Tmean,kind="line")
+    # plt.show()
+# print(data1,data2,data3,data4)
 end_time = time.time()
 print(end_time-start_time,'s')
