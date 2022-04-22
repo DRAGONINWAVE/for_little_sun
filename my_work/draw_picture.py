2fefee2402ca3aa9b0e65cc081ec3e82cc65cceb
DRAGONINWAVE <yl597591@gmail.com>
2022-04-22 19:23:00 +0800

    temperature_change

--- my_work/draw_picture.py
+++ my_work/draw_picture.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import time
 from scipy import stats
 from matplotlib.ticker import MaxNLocator
+# import unicode
 
 start_time = time.time()
 path = 'D:\TD\my_work\data1\\'
@@ -20,7 +21,7 @@ f,axes = plt.subplots(3,2,
                       sharex= True,
                     )
 plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
+# plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
 
 for ax in axes.flatten():
     s = df.columns[i]
@@ -34,36 +35,35 @@ for ax in axes.flatten():
     sns.lineplot(
         data=df,
         x = 'Year',y = s,
+        color = 'black',
+        # width = 5
         # kind="line",
         # ax = ax
+        lw = 3
                  )
     res = stats.linregress(list(df.Year),y)
     print(res)
-    # if k == 2 or k == 5 or k == 0 or k == 1:
-    #     p = sns.regplot(x='Year', y=s, data=df,ci=None,scatter=False,
-    #                 label=f'y={res.slope:.2f}*x{res.intercept:.2f} \n r\u00b2 ={res.rvalue**2:.2f}',
-    #                     # ,\n r = {res.rvalue:.2f}',
-    #                 # locals = 'right',
-    #                 ax=ax,
-    #                 color = 'black'
-    #                 )
-    # else:
     p = sns.regplot(x='Year', y=s, data=df,ci=None,scatter=False,
-                label=f'y={res.slope:.2f}*x{res.intercept:.2f} \n R^2 ={res.rvalue**2:.2f}',
+                label=f'y={res.slope:.2f}x{res.intercept:.2f} \n R\u00b2 ={res.rvalue**2:.2f}',
                     # ,\n r = {res.rvalue:.2f}',
+
                 # locals = 'right',
                 ax=ax,
-                color = 'black'
+                color = 'black',
+                # ls='-'
+                # lw = 10,
                 )
-    p.legend(loc = 'upper right',fontsize = 80)
+    font1 = {'family': 'Times New Roman','size':80}
+    p.legend(loc = 'upper right',fontsize = 80,prop=font1)
+
     i = i + 1
     ax.tick_params(labelsize=80,tickdir='in', length=30, width=5, colors='black',bottom='True',left='True',
                grid_color='r', grid_alpha=1)
     # ax.tick_params(axis='x', labelsize=80,direction='in', length=6, width=2, colors='black',
     #            grid_color='r', grid_alpha=1)
     # plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
-    plt.xlabel('年代',fontsize = 80)
-    plt.ylabel(ls[k]+'℃',fontsize = 80)
+    plt.xlabel(u'年代',fontsize = 80)
+    plt.ylabel(ls[k]+u'℃',fontsize = 80)
     # ax.arrow(0,12,0,1,length_includes_head= True)
 
     # ax.arrow(y, 0, 0., fc='k', ec='k',
@@ -71,7 +71,8 @@ for ax in axes.flatten():
     k = k + 1
 # ax.tick_params( labelsize=80,tickdir='in', length=6, width=2, colors='black',
 #        grid_color='r', grid_alpha=1)
-f.subplots_adjust(0.05,0.04,1,1,0.09,0.00)
+# f.plots_adjust(*,*,*,*,*,0.09)
+f.subplots_adjust(0.05,0.04,1,0.99,0.09,0.00)
 kdeplot_fig = f.get_figure()
 kdeplot_fig.savefig(str(time.strftime("%Y%m%d%H%M%S")),dpi = 200)
 end_time = time.time()
