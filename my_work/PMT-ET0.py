edc61e1f6409ce848d0904398c482cc14b4ea79e
DRAGONINWAVE <yl597591@gmail.com>
2022-04-29 15:42:15 +0800

    modify .py

--- my_work/PMT-ET0.py
+++ my_work/PMT-ET0.py
@@ -13,6 +13,7 @@ for name in names:
     ea = []
     df_raw = pd.read_excel('D:\TD\my_work\data\\'+name)
     k = 0
+    es = []
     for AI in df_raw.AI:
         if AI >= 1.00:
             ea.append(float(0.611*math.exp(17.27*(df_raw.Tmean.values[k] - 2)/(df_raw.Tmean.values[k] - 2 + 273.3))))
@@ -24,32 +25,38 @@ for name in names:
             ea.append(float(0.611*math.exp(17.27*(df_raw.Tmin.values[k] - 2)/(df_raw.Tmin.values[k] - 2 + 273.3))))
         if AI<0.05:
             ea.append(float(0.611*math.exp(17.27*(df_raw.Tmin.values[k] - 4)/(df_raw.Tmin.values[k] - 4 + 273.3))))
-        k = k + 1
+
     # print(len(Tdew))
-    es = (0.611*np.exp(17.27*df_raw.Tmin/(df_raw.Tmin+273.3))+0.611*np.exp(17.27*df_raw.Tmax/(df_raw.Tmax+273.3)))/2
+        es.append((0.611*np.exp(17.27*df_raw.Tmin.values[k]/(df_raw.Tmin.values[k]+273.3))+0.611*np.exp(17.27*df_raw.Tmax.values[k]/(df_raw.Tmax.values[k]+273.3)))/2)
     # print(type(ea),type(es))
+        k = k + 1
 
     df_raw.insert(df_raw.shape[1],'ea',ea)
     df_raw.insert(df_raw.shape[1],'es',es)
     VPD = df_raw.es - df_raw.ea
     df_raw.insert(df_raw.shape[1],'VPD',VPD)
-
-    kRs_G = 0.3648 - 0.0099*(df_raw.Tmax - df_raw.Tmin) + 0.0194*2 - 0.0017*df_raw.RH
+    kRs_G = []
+    i = 0
+    for AI in df_raw.AI:
+        kRs_G.append(0.3648 - 0.0099*(df_raw.Tmax.values[i] - df_raw.Tmin.values[i]) + 0.0194*2 - 0.0017*df_raw.RH.values[i])
+        i = i + 1
     df_raw.insert(df_raw.shape[1],'kRs_G',kRs_G)
+    # print(kRs_G)
     # print(df_raw)
     j = 0
     kRs_CF = []
     for AI in df_raw.AI:
         if AI >= 1.00:
-            kRs_CF.append(float(0.5191 - 0.0104*(df_raw.Tmin.values[j] - df_raw.Tmax.values[j]) + 0.0188*2 - 0.0035*df_raw.RH.values[j]))
+            kRs_CF.append(float(0.5191 - 0.0104*(df_raw.Tmax.values[j] - df_raw.Tmin.values[j]) + 0.0188*2 - 0.0035*df_raw.RH.values[j]))
         if 0.65<=AI<1:
-            kRs_CF.append(float(0.3958 - 0.0105*(df_raw.Tmin.values[j] - df_raw.Tmax.values[j]) + 0.0186*2 - 0.0021*df_raw.RH.values[j]))
+            kRs_CF.append(float(0.3958 - 0.0105*(df_raw.Tmax.values[j] - df_raw.Tmin.values[j]) + 0.0186*2 - 0.0021*df_raw.RH.values[j]))
         if 0.20<=AI<0.65:
-            kRs_CF.append(float(0.3880 - 0.0095*(df_raw.Tmin.values[j] - df_raw.Tmax.values[j]) + 0.0224*2 - 0.0022*df_raw.RH.values[j]))
+            kRs_CF.append(float(0.3880 - 0.0095*(df_raw.Tmax.values[j] - df_raw.Tmin.values[j]) + 0.0224*2 - 0.0022*df_raw.RH.values[j]))
         if AI<0.20:
-            kRs_CF.append(float(0.2169 - 0.0042*(df_raw.Tmin.values[j] - df_raw.Tmax.values[j]) + 0.0352*2 - 0.0011*df_raw.RH.values[j]))
-        k = j + 1
+            kRs_CF.append(float(0.2169 - 0.0042*(df_raw.Tmax.values[j] - df_raw.Tmin.values[j]) + 0.0352*2 - 0.0011*df_raw.RH.values[j]))
+        j = j + 1
     df_raw.insert(df_raw.shape[1],'kRs_CF',kRs_CF)
+    print(kRs_CF)
     Rs_G = df_raw.kRs_G*(df_raw.Tmax-df_raw.Tmin)**0.5*df_raw.Ra
     Rs_CF = df_raw.kRs_CF*(df_raw.Tmax-df_raw.Tmin)**0.5*df_raw.Ra
     df_raw.insert(df_raw.shape[1],'Rs_G',Rs_G)
