f7472110cad2f2883b0fa0f1b30d0296eac6aa68
DRAGONINWAVE <yl597591@gmail.com>
2022-05-01 00:02:36 +0800

    modify .py

--- my_work/mean_all.py
+++ my_work/mean_all.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 
+from my_work.temperature_change import y_ET0
+
 filepath = r'D:\TD\my_work\data'
 # print(len(os.listdir(filepath)))
 for Filename in os.listdir(filepath):
@@ -23,53 +25,61 @@ for Filename in os.listdir(filepath):
         # y_Rs = 0
         # y_S = 0
         # y_N = 0
+        y_ET0 = 0
         # print(str(File.date[counts]).split('-'))
-        if i == int(str(File.date[counts]).split('-')[0]):
+        if i == int(str(File.year[counts])):
             # print(str(File.date[counts]).split('/')[0])
             # print(File.year[counts])
             for month in range(1,13):
-                # day = 0
+                day = 0
                 # m_Rs = 0
                 # m_S = 0
                 # m_N = 0
+                m_ET0 = 0
                 for days in range(1,32):
                     # print(str(File.date[counts]).split('-')[1])
-                    if month == int(str(File.date[counts]).split('-')[1]):
+                    if month == int(str(File.month[counts])):
                         # print((str(File.date[counts]).split('-')[:]))
                         # m_Rs = m_Rs + File.RS[counts]
                         # # print(m_Rs)
                         # m_S = m_S + File.S[counts]
                         # m_N = m_N + File.N[counts]
+                        m_ET0 = m_ET0 + File.ET0_PMT[counts]
                         counts = counts + 1
                         day = day + 1
                     else:
                         break
-            #     RSmonth_mean = m_Rs / day
-            #     Smonth_mean = m_S / day
-            #     Nmonth_mean = m_N / day
-            #     y_Rs = y_Rs + RSmonth_mean
-            #     y_S = y_S + Smonth_mean
-            #     y_N = y_N + Nmonth_mean
-            #     lRs.append(RSmonth_mean)
-            #     lS.append(Smonth_mean)
-            #     lN.append(Nmonth_mean)
-            # lyRs.append(y_Rs)
-            # lyS.append(y_S)
-            # lyN.append(y_N)
+                if day != 0:
+                #     RSmonth_mean = m_Rs / day
+                #     Smonth_mean = m_S / day
+                #     Nmonth_mean = m_N / day
+                    ET0_mean = m_ET0 / day
+                #     y_Rs = y_Rs + RSmonth_mean
+                #     y_S = y_S + Smonth_mean
+                #     y_N = y_N + Nmonth_mean
+                    y_ET0 = y_ET0 + ET0_mean
+                #     lRs.append(RSmonth_mean)
+                #     lS.append(Smonth_mean)
+                #     lN.append(Nmonth_mean)
+                    mET0.append(ET0_mean)
+                # lyRs.append(y_Rs)
+                # lyS.append(y_S)
+                # lyN.append(y_N)
+                yET0.append(y_ET0)
     month = list(range(1,13))*40
-    year = list(range(1977,2017))
+    year = list(range(1967,2017))
     month_mean = {'月':month,
-                  'ET0mean':lET0
+                  'ET0mean':yET0
                   # 'RSmonth_mean':lRs,'Smonth_mean':lS,'Nmonth_mean':lN
                   }
     # print(len(month),len(lRs),len(lS),len(lN))
-    year_sum = {'年':year,'y_ET0':ET0,
+    year_sum = {'年':year,'y_ET0':mET0,
                 # 'y_S':lyS,'y_N':lyN
                 }
     # print(year_sum)
     MM = pd.DataFrame(data=month_mean)
-    MM.to_excel('D:\TD\my_work\lh_月平均\\'+Filename.split('.')[0]+'月平均.xlsx',index=False)
+    MM.to_excel('D:\TD\my_work\\'+Filename.split('.')[0]+'月平均.xlsx',index=False)
     YM = pd.DataFrame(data=year_sum)
-    YM.to_excel('D:\TD\my_work\lh_年总和\\'+Filename.split('.')[0]+'年总和.xlsx',index=False)
+    YM.to_excel('D:\TD\my_work\\'+Filename.split('.')[0]+'年总和.xlsx',index=False)
     print(counts)
     # counts = 0
