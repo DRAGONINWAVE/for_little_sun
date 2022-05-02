871239863c432b7500b0dbfdd1c696ae2bcdf6b5
DRAGONINWAVE <yl597591@gmail.com>
2022-05-02 14:07:54 +0800

    modify .py

--- my_work/draw_picture.py
+++ my_work/draw_picture.py
@@ -21,7 +21,7 @@ f1,axes1 = plt.subplots(3,2,
                       # sharex= True,
                     )
 plt.rcParams['font.sans-serif']=['SimSun'] #用来正常显示中文标签
-# plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
+plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
 
 for ax in axes1.flatten():
     s = df.columns[i]
@@ -91,7 +91,7 @@ f2,axes2 = plt.subplots(3,2,
                       # sharex= True,
                     )
 plt.rcParams['font.sans-serif']=['SimSun'] #用来正常显示中文标签
-# plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
+plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
 k1  = 0
 for ax in axes2.flatten():
     s = df.columns[i]
