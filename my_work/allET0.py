19c68757d80e3fa1ab418b991c6aa4a45e278a3b
DRAGONINWAVE <yl597591@gmail.com>
2022-05-02 19:27:49 +0800

    modify .py

--- my_work/allET0.py
+++ my_work/allET0.py
@@ -10,18 +10,19 @@ import seaborn as sns
 names = os.listdir(r'D:\TD\my_work\data')
 # print(names)
 # all_ET0 = pd.DataFrame(ET0_PMT_CF=ET0_PMT_CF,)
-ET0_PMT_CF = 0
-ET0_PMT_G = 0
-ET0_PMT = 0
+ET0_PMT_CF = pd.Series()
+ET0_PMT_G = pd.Series()
+ET0_PMT = pd.Series()
 for name in names:
     print(name)
     # ea = []
     df_raw = pd.read_excel('D:\TD\my_work\data\\'+name)
     # print(type(df_raw.ET0_PMT_CF))
 
-    ET0_PMT_CF += df_raw.ET0_PMT_CF
-    ET0_PMT_G += df_raw.ET0_PMT_G
-    ET0_PMT += df_raw.ET0_PMT
+    ET0_PMT_CF = ET0_PMT_CF.append(df_raw.ET0_PMT_CF)
+    # print(type(df_raw.ET0_PMT_CF))
+    ET0_PMT_G = ET0_PMT_G.append(df_raw.ET0_PMT_G)
+    ET0_PMT = ET0_PMT.append(df_raw.ET0_PMT)
 
 
 all_data = pd.DataFrame(dict(ET0_PMT_CF=ET0_PMT_CF,ET0_PMT_G=ET0_PMT_G,ET0_PMT=ET0_PMT,))
@@ -29,6 +30,7 @@ all_data = pd.DataFrame(dict(ET0_PMT_CF=ET0_PMT_CF,ET0_PMT_G=ET0_PMT_G,ET0_PMT=E
 # plt.plot(all_data.ET0_PMT,all_data.ET0_PMT_G)
 # plt.show()
 res1 = stats.linregress(list(all_data.ET0_PMT_CF),list(all_data.ET0_PMT))
-print(res1,res1.rvalue**2,len(all_data),np.average(all_data.ET0_PMT_CF))
+print(res1,res1.rvalue**2,len(all_data),np.average(all_data.ET0_PMT_CF),len(all_data.ET0_PMT))
 res2 = stats.linregress(list(all_data.ET0_PMT_G),list(all_data.ET0_PMT))
 print(res2,res2.rvalue**2)
+all_data.to_excel('D:\TD\my_work\\all.xlsx',index = False)
