33e86fcf7082c2af8dcdbf5d0551d7ce8f4e65e1
DRAGONINWAVE <yl597591@gmail.com>
2022-05-22 09:28:08 +0800

    three ways to create show_count

--- Chapter8_gradual_typing/show_count.py
+++ Chapter8_gradual_typing/show_count.py
@@ -1,11 +1,19 @@
-#
+from typing import Optional
 # def show_count(count,word):
 #     if count == 1:
 #         return f'1 {word}'
 #     count_str = str(count) if count else 'no'
 #     return f'{count_str} {word}s'
 
-def show_count(count:int,singular:str,plural:str = '') ->str:
+# def show_count(count:int,singular:str,plural:str = '') ->str:
+#     if count == 1:
+#         return f'1 {singular}'
+#     count_str = str(count) if count else 'no'
+#     if not plural:
+#         plural = singular + 's'
+#     return f'{count_str} {plural}'
+
+def show_count(count:int,singular:str,plural:Optional[str] = None) ->str:
     if count == 1:
         return f'1 {singular}'
     count_str = str(count) if count else 'no'
