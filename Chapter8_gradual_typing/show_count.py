97aa24185ef9a67c4905ab3a3760888f6fabc5dd
DRAGONINWAVE <yl597591@gmail.com>
2022-05-22 09:23:23 +0800

    show_count

---
+++ Chapter8_gradual_typing/show_count.py
@@ -0,0 +1,17 @@
+#
+# def show_count(count,word):
+#     if count == 1:
+#         return f'1 {word}'
+#     count_str = str(count) if count else 'no'
+#     return f'{count_str} {word}s'
+
+def show_count(count:int,singular:str,plural:str = '') ->str:
+    if count == 1:
+        return f'1 {singular}'
+    count_str = str(count) if count else 'no'
+    if not plural:
+        plural = singular + 's'
+    return f'{count_str} {plural}'
+
+
+print(show_count(0,'bird'))
