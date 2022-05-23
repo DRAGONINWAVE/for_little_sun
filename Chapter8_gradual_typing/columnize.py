9dc37d5ab0eed50f732853ee9f3073f4878419cc
DRAGONINWAVE <yl597591@gmail.com>
2022-05-23 08:53:31 +0800

    columnize

---
+++ Chapter8_gradual_typing/columnize.py
@@ -0,0 +1,13 @@
+from collections.abc import Sequence
+
+def columnize(sequence:Sequence[str],num_columns:int = 0) -> list[tuple[str, ...]]:
+    if num_columns == 0:
+        num_columns = round(len(sequence)**.5)
+    num_rows,reminder = divmod(len(sequence),num_columns)
+    num_rows += bool(reminder)
+    return [tuple(sequence[i::num_rows]) for i in range(num_rows)]
+
+
+animals = 'drake fawn heron ibex koala lynx tahr xerus yak zapus'.split()
+table = columnize(animals)
+print(table)
