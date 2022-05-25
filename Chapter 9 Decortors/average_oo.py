e2f088f008c11a432ef2f8ab54b420487dcaa96f
DRAGONINWAVE <yl597591@gmail.com>
2022-05-25 10:01:13 +0800

    average an advanced version

--- Chapter 9 Decortors/average_oo.py
+++ Chapter 9 Decortors/average_oo.py
@@ -1,14 +1,30 @@
-class Averager():
+# class Averager():
+#
+#     def __init__(self):
+#         self.series = []
+#
+#     def __call__(self,new_value):
+#         self.series.append(new_value)
+#         total = sum(self.series)
+#         return total / len(self.series)
+#
+#
+# ave = Averager()
+# ave(10)
+# print(ave(11))
 
-    def __init__(self):
-        self.series = []
+#advanced way
+def make_averager():
+    series = []
 
-    def __call__(self,new_value):
-        self.series.append(new_value)
-        total = sum(self.series)
-        return total / len(self.series)
+    def averager(new_value):
+        series.append(new_value)
+        total = sum(series)
+        return total / len(series)
+
+    return averager
 
 
-ave = Averager()
-ave(10)
-print(ave(11))
+avg = make_averager()
+avg(10)
+print(avg(11))
