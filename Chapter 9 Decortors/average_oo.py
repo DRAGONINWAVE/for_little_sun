bd99606aef135b7bd2d339b6852c9ed3e2056350
DRAGONINWAVE <yl597591@gmail.com>
2022-05-26 09:45:02 +0800

    average an advanced version

--- Chapter 9 Decortors/average_oo.py
+++ Chapter 9 Decortors/average_oo.py
@@ -14,17 +14,42 @@
 # print(ave(11))
 
 #advanced way
+# def make_averager():
+#     series = []
+#
+#     def averager(new_value):
+#         series.append(new_value)
+#         total = sum(series)
+#         return total / len(series)
+#
+#     return averager
+#
+#
+# avg = make_averager()
+# avg(10)
+# print(avg(11),avg.__code__.co_freevars,avg.__closure__[0].cell_contents)
+#
+
+
+
+#Calculate a running average without keeping all history (fixed with the use of nonlocal)
 def make_averager():
-    series = []
+    count = 0
+    total = 0
 
     def averager(new_value):
-        series.append(new_value)
-        total = sum(series)
-        return total / len(series)
+        nonlocal count,total
+        count += 1
+        total += new_value
+        return  total/count
 
     return averager
 
-
 avg = make_averager()
 avg(10)
-print(avg(11))
+print(avg(11),avg.__code__.co_freevars,avg.__closure__[0].cell_contents)
+
+
+
+
+
