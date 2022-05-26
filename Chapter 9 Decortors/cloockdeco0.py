833b3d0bf435057e4c75558893b23cdaadbbe028
DRAGONINWAVE <yl597591@gmail.com>
2022-05-26 10:53:58 +0800

    clock

---
+++ Chapter 9 Decortors/cloockdeco0.py
@@ -0,0 +1,12 @@
+import time
+
+def clock(func):
+    def clocked(*args):
+        t0 = time.perf_counter()
+        result = func(*args)
+        elapsed = time.perf_counter() - t0
+        name =func.__name__
+        arg_str = ', '.join(repr(arg) for arg in args)
+        print(f'[{elapsed:0.8f}s]{name}({arg_str}) -> {result!r}')
+        return result
+    return clocked
