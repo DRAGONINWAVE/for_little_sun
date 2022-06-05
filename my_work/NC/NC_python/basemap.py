9e6f57328c74b5cc0463be3bba2ad7cf3ed7ef68
DRAGONINWAVE <yl597591@gmail.com>
2022-06-05 15:11:06 +0800

    nepal statesmap

--- my_work/NC/NC_python/basemap.py
+++ my_work/NC/NC_python/basemap.py
@@ -5,11 +5,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 plt.figure(1)
 
-# map=Basemap()
+
 map=Basemap(llcrnrlon=79,llcrnrlat=26,urcrnrlon=90,urcrnrlat=31)
-# map.drawcoastlines()
-# map.drawcountries()
-# map.drawrivers(color='black',linewidth=0.3)
 CHN = r'F:\Nepal_ET0'
 map.readshapefile(CHN+'\\NEPAL_BOUNDARIES\\npl_admbnda_adm0_nd_20201117',
 'country',drawbounds=True)
