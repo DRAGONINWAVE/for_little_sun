eea3e938c0dfe568df1c3f96d06afad934d5d8c2
DRAGONINWAVE <yl597591@gmail.com>
2022-06-09 21:22:11 +0800

    busy day

--- help_others/qianqinhao/months_mean.py
+++ help_others/qianqinhao/months_mean.py
@@ -1,93 +1,56 @@
 import pandas as pd
 import os
 
-filepath = r'D:\TD\help_others\qianqinhao\二步处理'
-# print(len(os.listdir(filepath)))
-# for Filename in os.listdir(filepath):
-    # print(Filename)
-Filename = 'cleared_dropna'
-File = pd.read_excel('D:\TD\help_others\qianqinhao\\'+ Filename +'.xlsx')
-names = File.columns.values
-print(Filename)
-print(names)
-lRs = []
-lS = []
-lN = []
-lTmean = []
-lyRs = []
-lyS = []
-lyN = []
-lyTmean = []
+# from my_work.temperature_change import s1_T, data0
 
+filepath = r'D:\TD\help_others\qianqinhao\二步处理\\'
+Filename = 'cleared_dropna'
+df = pd.read_excel(filepath+ Filename +'.xlsx')
+names = df.columns.values
+print(names)
 counts = 0
+s23_T = 0
+m78_T = 0
+average_s23_T = []
+average_7_8 = []
+average_y_T = []
+year = []
+season23 = list(range(4, 10))
+season7_8 = list(range(7,9))
+print(df)
 for i in range(1951,2018):
-    # print(Filename)
-    # y_Rs = 0
-    # y_S = 0
-    # y_N = 0
-    y_Tmean = 0
-    # print(str(File.date[counts]).split('-'))
-    if i == int(str(File.年[counts])):
-        # print(str(File.date[counts]).split('/')[0])
-        # print(File.year[counts])
-        for month in range(4,10):
-            day = 0
-            # m_Rs = 0
-            # m_S = 0
-            # m_N = 0
-            m_Tmean = 0
-            for days in range(1,32):
-                # print(str(File.date[counts]).split('-')[1])
-                if month in int(str(File.月[counts])):
-                    # print((str(File.date[counts]).split('-')[:]))
-                    # m_Rs = m_Rs + File.RS[counts]
-                    # print(m_Rs)
-                    # m_S = m_S + File.S[counts]
-                    # m_N = m_N + File.N[counts]
-                    m_Tmean = m_Tmean+ File.平均[counts]
-                    counts = counts + 1
-                    day = day + 1
-                else:
-                    break
-            if day != 0:
-                # RSmonth_mean = m_Rs / day
-                # Smonth_mean = m_S / day
-                # Nmonth_mean = m_N / day
-                Tmonth_mean = m_Tmean / day
-                # y_Rs = y_Rs + RSmonth_mean
-                # y_S = y_S + Smonth_mean
-                # y_N = y_N + Nmonth_mean
-                y_Tmean = y_Tmean + Tmonth_mean
-                # lRs.append(RSmonth_mean)
-                # lS.append(Smonth_mean)
-                # lN.append(Nmonth_mean)
-                lTmean.append(Tmonth_mean)
-        # lyRs.append(y_Rs)
-        # lyS.append(y_S)
-        # lyN.append(y_N)
-        lyTmean.append(y_Tmean/12)
-month = list(range(1,13))*40
-year = list(range(1951,2018))
-month_mean = {
-              # '月':month,
-              # 'RSmonth_mean':lRs,
-              # 'Smonth_mean':lS,
-              # 'Nmonth_mean':lN,
-              '月平均气温':lTmean
-              }
-# print(len(month),len(lRs),len(lS),len(lN))
-year_sum = {
-            '年':year,
-            # 'y_Rs':lyRs,
-            # 'y_S':lyS,
-            # 'y_N':lyN
-            '年平均气温':lyTmean
-            }
-# print(year_sum)
-print(len(lTmean),len(lyTmean))
-MM = pd.DataFrame(data=month_mean)
-MM.to_excel('D:\TD\help_others\qianqinhao\二步处理\\'+Filename+'月平均.xlsx')
-# YM = pd.DataFrame(data=year_sum)
-# YM.to_excel('D:\TD\help_others\qianqinhao\二步处理\\'+Filename+'年总和.xlsx',index=False)
-print(counts)
-# counts = 0
+    y_T = 0
+    day = 0
+    day23 = 0
+    day78 = 0
+    for days in range(1,31*12):
+        if i ==df.年[counts]:
+            y_T = y_T + df.平均[counts]
+            if df.月[counts] in season23:
+                s23_T = s23_T + df.平均[counts]
+                day23 = day23 + 1
+            if df.月[counts] in season7_8:
+                m78_T = m78_T + df.平均[counts]
+                day78 = day78 + 1
+            day = day + +1
+            counts = counts + 1
+        if counts == len(df.年):
+            break
+
+    if day != 0 and day23 != 0:
+        y_T = y_T /day
+        s23_T = s23_T /day23
+        m78_T = m78_T / day78
+
+        average_y_T.append(y_T)
+        average_s23_T.append(s23_T)
+        average_7_8.append(m78_T)
+        year.append(df.年.values[counts - 1])
+
+data0 = pd.DataFrame(dict(Year=year,Tmean=average_y_T,S23=average_s23_T,M78=average_7_8))
+print(data0)
+data0.to_excel('YearMean.xlsx',index=False)
+print('ok')
+
+
+
---
+++ spider/run2.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import csv
+import os
+import lxml
+import re
+import time
+import pandas
+hh = []
+data = pandas.read_excel('diming6.9.xlsx')
+locations = data.iloc[:,0].values
+# print(locations)
+for location in locations:
+    # print(location)
+    for j in range(2015,2020):
+        yy = str(j)
+        for i in range(1,12+1):
+            # if j == 2019 and i==12:
+            #     break
+            # else:
+            print(location,j,i)
+            mm = str(i).zfill(2)
+            r = requests.get(url="http://www.tianqihoubao.com/lishi/"+location+"/month/"+yy+mm+".html")
+            soup = BeautifulSoup(r.text,'lxml')
+            tds = soup.find_all("tr")
+            for i in tds:
+                hh.append(i.get_text().replace("\r\n", "").replace("\n\n", "").replace("\n", "").replace(" ", ""))
+    data = pandas.DataFrame({location:hh})
+    data.to_excel(location+"2015-2019.xlsx",index=False,encoding="gb2312")
