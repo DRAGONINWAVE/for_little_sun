8c77cc3cd75b0cae9075fd802faab714affe1674
DRAGONINWAVE <yl597591@gmail.com>
2022-06-10 10:01:12 +0800

    busy day

--- help_others/qianqinhao/clear1.py
+++ help_others/qianqinhao/clear1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-raw_data = pd.read_excel('D:\TD\help_others\qianqinhao\\56875站数据.xlsx')
-cleared_data = raw_data.dropna()
-cleared_data.to_excel('cleared_dropna.xlsx',index=False)
+raw_data = pd.read_excel('D:\TD\help_others\qianqinhao\\56875站数据.xlsx',sheet_name='降水')
+raw_data1 = raw_data.drop(raw_data[raw_data['20-20时累计降水量']>30000].index)
+cleared_data = raw_data1.dropna()
+cleared_data.to_excel('P_cleared_dropna.xlsx',index=False)
 print(cleared_data)
--- help_others/qianqinhao/months_mean.py
+++ help_others/qianqinhao/months_mean.py
@@ -14,24 +14,36 @@ m78_T = 0
 average_s23_T = []
 average_7_8 = []
 average_y_T = []
+day_all_35_y = []
+day78_35_y = []
+AT_49_y = []
 year = []
 season23 = list(range(4, 10))
 season7_8 = list(range(7,9))
 print(df)
 for i in range(1951,2018):
     y_T = 0
+    AT_49  = 0
     day = 0
     day23 = 0
     day78 = 0
+    day_all_35 = 0
+    day78_35 = 0
     for days in range(1,31*12):
         if i ==df.年[counts]:
             y_T = y_T + df.平均[counts]
+            if df.最高[counts]>=35:
+                day_all_35 = day_all_35 + 1
             if df.月[counts] in season23:
                 s23_T = s23_T + df.平均[counts]
                 day23 = day23 + 1
             if df.月[counts] in season7_8:
                 m78_T = m78_T + df.平均[counts]
                 day78 = day78 + 1
+                if df.最高[counts] >= 35:
+                    day78_35 = day78_35 + 1
+                if df.平均[counts] >= 13:
+                    AT_49 = AT_49 + df.平均[counts]
             day = day + +1
             counts = counts + 1
         if counts == len(df.年):
@@ -46,10 +58,24 @@ for i in range(1951,2018):
         average_s23_T.append(s23_T)
         average_7_8.append(m78_T)
         year.append(df.年.values[counts - 1])
+        day_all_35_y.append(day_all_35)
+        AT_49_y.append(AT_49)
+        day78_35_y.append(day78_35)
 
-data0 = pd.DataFrame(dict(Year=year,Tmean=average_y_T,S23=average_s23_T,M78=average_7_8))
+
+data0 = pd.DataFrame(dict(
+                          年=year,
+                          年平均气温=average_y_T,
+                          四到九月平均气温=average_s23_T,
+                          七八月平均气温=average_7_8,
+                          日最高气温大于35度日数=day_all_35_y,
+                          四到九月大于13度的积温=AT_49_y,
+                          七八月大于35度日数=day78_35_y,
+
+
+                    ))
 print(data0)
-data0.to_excel('YearMean.xlsx',index=False)
+data0.to_excel('YearMean2.xlsx',index=False)
 print('ok')
 
 
