5d107daf3acd9fb89c0dec3de75a13e4fb5bddad
DRAGONINWAVE <yl597591@gmail.com>
2022-06-11 10:10:40 +0800

    busy day

---
+++ my_work/spider/first_aiohttp_houbao.py
@@ -0,0 +1,25 @@
+import aiohttp
+import asyncio
+
+#test footprint
+# async def main():
+#
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get('http://python.org') as response:
+#
+#             print("Status:", response.status)
+#             print("Content-type:", response.headers['content-type'])
+#
+#             html = await response.text()
+#             print("Body:", html[:15], "...")
+#
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(main())
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://httpbin.org/get') as resp:
+            print(resp.status)
+            print(await resp.text())
+
+asyncio.run(main)
