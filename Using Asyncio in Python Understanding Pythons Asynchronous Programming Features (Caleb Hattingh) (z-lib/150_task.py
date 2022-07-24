659f3aec8962cac2e0730f182239068095aa945f
DRAGONINWAVE <yl597591@gmail.com>
2022-07-24 16:34:05 +0800

    async

---
+++ Using Asyncio in Python Understanding Pythons Asynchronous Programming Features (Caleb Hattingh) (z-lib/150_task.py
@@ -0,0 +1,33 @@
+import aiohttp
+import asyncio
+import time
+
+# 抓取指定編號的網址
+async def get_pokemon(session, number):
+    url = f'https://pokeapi.co/api/v2/pokemon/{number}'
+    await asyncio.sleep(1)
+    async with session.get(url) as resp:
+        pokemon = await resp.json()
+        return "%d：%s" % (number, pokemon['name'])
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        # 建立 Task 列表
+        tasks = []
+        for number in range(1, 151):
+            tasks.append(asyncio.create_task(get_pokemon(session, number)))
+
+        # 同時執行所有 Tasks
+        original_pokemon = await asyncio.gather(*tasks)
+
+        # 輸出結果
+        for pokemon in original_pokemon:
+            print(pokemon)
+
+start = time.perf_counter() # 開始測量執行時間
+
+# 執行協同程序
+asyncio.run(main())
+
+elapsed = time.perf_counter() - start # 計算程式執行時間
+print("執行時間：%f 秒" % elapsed)
