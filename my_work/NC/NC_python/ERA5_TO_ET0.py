09dc40f0f13c2d654def01a18188d9437dedeae7
DRAGONINWAVE <yl597591@gmail.com>
2022-07-30 10:01:58 +0800

    NC basic handle

--- my_work/NC/NC_python/ERA5_TO_ET0.py
+++ my_work/NC/NC_python/ERA5_TO_ET0.py
@@ -17,28 +17,28 @@ def FILENAMES(start, end):
             filenames.append(str(year)+month+'.nc')
     return filenames
 #计算每月的平均温度
+
 def MEAN_TEMP(path2,filenames):
     f1 = xr.Dataset()
     for filename in tqdm(filenames,desc='computing mean temperature'):
         f = xr.open_dataset(path + path2 + filename)
-        f1 = xr.concat([f],dim=['time'])
+        f1 = xr.merge([f1,f])
     return f1
-# def test_MEAN_TEMP(path2,filenames):
+
 def CONCAT_ARRAYS(filenames):
     xadv_new = []
     for i in tqdm(range(len(filenames)),desc='concating arrays'):
         f0 = xr.open_dataset(path + path2 + filenames[i])
-        xadv_new.append(f0)
+        xadv_new.append(f0['t2m'])
     f1 = xr.concat(xadv_new,dim=['time'])
     return f1
+
 def main():
     filenames = FILENAMES(2000,2020)
-    TEMP = MEAN_TEMP('temperature\mean\\',filenames)
-    concat_files = CONCAT_ARRAYS(filenames)
-    print(concat_files)
+    TEMP = MEAN_TEMP(path2,filenames)
+    # concat_files = CONCAT_ARRAYS(filenames)
+    print(len(TEMP['t2m'].mean(dim=['time'])))
+    # print(concat_files)
+
 if __name__ == '__main__':
     main()
-
-
-
-
