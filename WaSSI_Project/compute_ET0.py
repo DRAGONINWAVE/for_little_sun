e39d2c07ddd801aba0d267845882c0238834f4ea
DRAGONINWAVE <yl597591@gmail.com>
2022-08-08 11:05:51 +0800

    compute e

--- WaSSI_Project/compute_ET0.py
+++ WaSSI_Project/compute_ET0.py
@@ -5,5 +5,7 @@ import xarray as xr
 data = xr.open_dataset(r'D:\TD\my_work\NC\NC_python\M2000_2020mean_mean.nc')
 TEMP = data['t2m'] - 273.3
 e = 6.108*np.exp(17.2693882*TEMP/(TEMP+237.3))
-e.to_netcdf('x.nc')
-print(xr.open_dataset(r'x.nc')['t2m'].values)
+# e.to_netcdf('x.nc')
+# print(xr.open_dataset(r'x.nc')['t2m'].values)
+rou_W = 216.7*e/(TEMP + 273.3)
+print(rou_W)
