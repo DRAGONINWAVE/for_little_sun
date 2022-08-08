4db6fb133b0b8dc40181cbc7d67399d0f158eb71
DRAGONINWAVE <yl597591@gmail.com>
2022-08-08 08:50:43 +0800

    NC basic handle

--- my_work/NC/NC_python/ERA5_TO_ET0.py
+++ my_work/NC/NC_python/ERA5_TO_ET0.py
@@ -4,7 +4,7 @@ import pandas as pd
 from tqdm import tqdm
 #setting the all_used path
 global path
-path = r'F:\ERA5\ERA5\\'
+path = r'F:\ERA5_TR\ERA5\\'
 global path2
 path2 = 'temperature\mean\\'
 #create the filenames will be used
@@ -21,7 +21,7 @@ def FILENAMES(start, end):
 #合并所有的nc文件,并求出各年每月平均
 def MEAN_TEMP(read_path,filenames,value,name):
     f1 = xr.Dataset()
-    for filename in tqdm(filenames,desc='computing mean temperature'):
+    for filename in tqdm(filenames,desc=read_path):
         f  = xr.open_dataset(path + read_path + filename)
         f1 = xr.merge([f,f1])
     f2 = f1[value]
@@ -30,9 +30,15 @@ def MEAN_TEMP(read_path,filenames,value,name):
     return f2
 
 def main():
-    # filenames = FILENAMES(start=2000,end=2020)
-    # TEMP  = MEAN_TEMP(path2,filenames,'t2m',name='M2000_2020mean_mean.nc')
-    # print(TEMP)
+    filenames = FILENAMES(start=2000,end=2020)
+    TEMP  = MEAN_TEMP(path2,filenames,'t2m',name='M2000_2020mean_mean.nc')
+    print(TEMP)
+    path3 = r'temperature\max\\'
+    TMAX = MEAN_TEMP(path3,filenames,'t2m',name='M2000_2020max_mean.nc')
+    print(TMAX)
+    path4 = r'temperature\min\\'
+    TMIN = MEAN_TEMP(path4,filenames,'t2m',name='M2000_2020min_mean.nc')
+    print(TMIN)
     # print(pd.date_range(start='2000-1', end='2022-12', freq='M'))
 
 if __name__ == '__main__':
