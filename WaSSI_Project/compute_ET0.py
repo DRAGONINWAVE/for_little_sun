a6357251f9eaee9eb5114b3220a413c392d90382
DRAGONINWAVE <yl597591@gmail.com>
2022-08-09 10:22:46 +0800

    compute e

--- WaSSI_Project/compute_ET0.py
+++ WaSSI_Project/compute_ET0.py
@@ -4,12 +4,12 @@ import xarray as xr
 import datetime
 
 data = xr.open_dataset(r'D:\TD\my_work\NC\NC_python\M2000_2020mean_mean.nc')
-# TEMP = data['t2m'] - 273.3
+TEMP = data['t2m'] - 273.3
 # data = data.isel(time=(data.time.dt.month.isin([1, 2, 3])))
-print(data.groupby("time.day").mean())
+print(xr.cftime_range(start="2000", periods=6, freq="1MS", calendar="julian"))
 # print(d)
 fmt = '%Y-%m-%d'
 # dt = datetime.datetime.strptime(d,fmt)
 # print(dt)
-# e = 6.108*np.exp(17.2693882*TEMP/(TEMP+237.3))
-# rou_W = 216.7*e/(TEMP + 273.3)
+e = 6.108*np.exp(17.2693882*TEMP/(TEMP+237.3))
+rou_W = 216.7*e/(TEMP + 273.3)
