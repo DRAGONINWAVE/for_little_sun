89f1a757b073a252f09075bd267ea9b62ad640dd
DRAGONINWAVE <yl597591@gmail.com>
2022-08-10 11:30:58 +0800

    compute ET

--- WaSSI_Project/compute_ET0.py
+++ WaSSI_Project/compute_ET0.py
@@ -10,7 +10,10 @@ def main():
     e = 6.108*np.exp(17.2693882*TEMP/(TEMP+237.3))
     rou_W = 216.7*e/(TEMP + 273.3)
     data['time.day'] = (data['time.day']/2)
-    print((np.ceil(data['time.dayofyear']-data['time.day'])).astype(int))
+    julian_date = (np.ceil(data['time.dayofyear']-data['time.day'])).astype(int)
+    # print(julian_date)
+    SIGMA = 0.4093 * np.sin(2*np.pi*julian_date/365 - 1.405)
+    print(SIGMA)
 
 if __name__ == '__main__':
     main()
