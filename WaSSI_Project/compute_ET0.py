5bbefc2077f966b84de6e2983a0708b86cf187ad
DRAGONINWAVE <yl597591@gmail.com>
2022-08-12 10:45:07 +0800

    compute ET

--- WaSSI_Project/compute_ET0.py
+++ WaSSI_Project/compute_ET0.py
@@ -55,13 +55,13 @@ def main():
 
     ## 计算日照时长：K，单位：12h
     K = 2 * OUMIGA / np.pi
-    print(K)
+    print(K.shape)
 
     ## 计算PET，（PEThamon）
     N_R = (data['time.dayofyear'] * rou_W)
     # print(data)
     PEThamon = 0.1651 * N_R[:,:,0] * K
-    print(PEThamon.shape)
+    print(PEThamon)
 
 
 if __name__ == '__main__':
