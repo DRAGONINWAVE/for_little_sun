f2dd45c37aeac97f99b989c7027738defdf80917
DRAGONINWAVE <yl597591@gmail.com>
2022-08-12 15:40:45 +0800

    Pandas Foundations

--- 《pandas_1.x_cookbook》/Read_csv.py
+++ 《pandas_1.x_cookbook》/Read_csv.py
@@ -7,3 +7,13 @@ print(movie.index)
 print(movie.dtypes)
 print(movie.dtypes.value_counts)
 print(movie.info)
+
+# the movie['sentiment'] is same as the movie.sentiment
+print(movie['sentiment'],movie.sentiment)
+
+# the functions of loc and iloc
+# loc
+print('movie.loc',movie.loc[:,'sentiment'])
+print('movie.iloc',movie.iloc[:,1])
+
+#
