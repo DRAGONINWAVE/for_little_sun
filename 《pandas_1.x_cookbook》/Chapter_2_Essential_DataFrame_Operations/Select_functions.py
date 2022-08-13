3b72e3f0a17846e17c27730e9b8fb8d40e8c58e1
DRAGONINWAVE <yl597591@gmail.com>
2022-08-13 11:23:34 +0800

    Pandas Foundations

--- 《pandas_1.x_cookbook》/Chapter_2_Essential_DataFrame_Operations/Select_functions.py
+++ 《pandas_1.x_cookbook》/Chapter_2_Essential_DataFrame_Operations/Select_functions.py
@@ -2,6 +2,19 @@ import pandas as pd
 import numpy as np
 
 movies = pd.read_csv(r'D:\TD\《pandas_1.x_cookbook》\ratings.csv')
-# print(movies.columns)
+print(movies.columns)
 # print(movies.select_dtypes(include=int).head())
-print(movies.select_dtypes(include=['number','float','object']).head())
+# print(movies.select_dtypes(include=['number','float','object']).head())
+
+# filter and like=
+# print(movies.filter(like='D').head())
+
+col = ['URL','description']
+# print(movies.filter(items=col).head())
+# abbr. 正则表达式（regular expression）
+# print(movies.filter(regex='d').head())
+# print(movies.shape)
+# print(movies.ndim)
+# print(movies.count())
+print(movies.describe(percentiles=[0.44,0.22,0.99]).T)
+print(movies.min(skipna=False))
