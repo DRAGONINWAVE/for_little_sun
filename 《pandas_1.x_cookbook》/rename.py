5754e62c899246c3e053d01bb387e37f32b053db
DRAGONINWAVE <yl597591@gmail.com>
2022-08-13 10:16:18 +0800

    Pandas Foundations

--- 《pandas_1.x_cookbook》/rename.py
+++ 《pandas_1.x_cookbook》/rename.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
 # .rename
+from pandas import Index
+
 movies = pd.read_csv('ratings.csv')
 # print(movies.columns)
-col = {'Directors':'directors'}
+col = {'Directors': 'directors'}
+print(type(col))
 movies = movies.rename(columns=col)
 # print(movies.columns)
 
@@ -14,6 +17,13 @@ columns = movies.columns.to_list()
 
 # add a column,method 1
 movies['has_seen'] = 0
-print(movies)
+# print(movies)
 # add a column,method 2
-print(movies.assign(has_seen1=0))
+# print(movies.assign(has_seen1=0))
+
+x = []
+for c in movies.columns:
+    x.append(c.lower())
+print(x)
+movies.columns = x
+print(movies.columns)
