7005be7456ec3bdefc3c07fb8bae465d95924947
DRAGONINWAVE <yl597591@gmail.com>
2022-08-14 14:40:14 +0800

    Pandas Foundations

--- 《pandas_1.x_cookbook》/Chapter_3_Creating_and_Persisting_DataFrame/nlargest_nsmallest.py
+++ 《pandas_1.x_cookbook》/Chapter_3_Creating_and_Persisting_DataFrame/nlargest_nsmallest.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 movies = pd.read_csv(r'D:\TD\《pandas_1.x_cookbook》\ratings.csv')
-print(movies[['Directors','IMDb Rating','Year']].nlargest(5,'IMDb Rating').nsmallest(10,'Year'))
+# print(movies[['Directors','IMDb Rating','Year']].nlargest(5,'IMDb Rating').nsmallest(10,'Year'))
+
+print(movies[['Directors','IMDb Rating','Year']].nlargest(1000,'IMDb Rating').tail())
