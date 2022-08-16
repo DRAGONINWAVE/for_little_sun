63d478bfad355b34ce48d6fe9bfcd3307086595e
DRAGONINWAVE <yl597591@gmail.com>
2022-08-16 13:49:00 +0800

    Panadas Foundations

--- 《pandas_1.x_cookbook》/Chapter_4_Explotary_Data_Analysis/Pandas_Pipe.py
+++ 《pandas_1.x_cookbook》/Chapter_4_Explotary_Data_Analysis/Pandas_Pipe.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 vehicle = pd.read_csv(r'D:\迅雷下载\vehicles.csv.zip')
 
@@ -17,3 +18,12 @@ fig.savefig(
     "c5-conthistpan.png", dpi=300
 )
 
+fig1 ,ax1 = plt.subplots(figsize=(10,8))
+sns.distplot(
+    vehicle.city08,
+    rug=True,
+    ax=ax1
+)
+fig1.savefig(
+    'c5-distplot.png', dpi=300
+)
