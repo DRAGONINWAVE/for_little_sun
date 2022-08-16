6da2ca63b60503dbb9312346561e6a8e7edd8c5f
DRAGONINWAVE <yl597591@gmail.com>
2022-08-16 08:53:29 +0800

    Panadas Foundations

--- 《pandas_1.x_cookbook》/Chapter_3_Creating_and_Persisting_DataFrame/TSLA.py
+++ 《pandas_1.x_cookbook》/Chapter_3_Creating_and_Persisting_DataFrame/TSLA.py
@@ -2,4 +2,9 @@ import pandas as pd
 
 tsla = pd.read_csv(r'D:\TD\《pandas_1.x_cookbook》\TSLA.csv')
 
-print(len(tsla))
+print(tsla.columns)
+
+tsla_close  = tsla['Close']
+tsla_cummax = tsla_close.cummax()
+print(tsla_close.head(5))
+print(tsla_cummax.head(5))
